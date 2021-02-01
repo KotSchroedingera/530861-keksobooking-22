c3fa69f3aaaa6be6e36388b5f8733cf4b3d0c0e7
kotschroedingera <79150323671@ya.ru>
2021-02-01 17:02:50 +0300

    исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 'use strict';
 
-/* Returns random integer number from min to max, incl. min and max
-* min, max = numbers
-* returns 'error' if false input (min > max; not numbers)
-* https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
-let getRandomIntInclusive = function (min, max) {
+
+const getRandomIntInclusive = function (min, max) {
   if (typeof max !== 'number' || typeof min !== 'number') return 'error';
+  if (min < 0) return 'error';
 
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -17,26 +14,23 @@ let getRandomIntInclusive = function (min, max) {
 }
 
 
-/* Returns float number form min to max, incl. min and max
-* min, max: numbers; signs = number of signs after comma
-* return 'error' if false input (min > max; not numbers)
-* https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
-let getRandomFloat = function(min, max, signs = 0) {
+const getRandomFloat = function(min, max, signs = 0) {
   if (typeof max !== 'number' || typeof min !== 'number' || typeof signs !== 'number') return 'error';
-  if (min > max) return 'error';
+  if (min < 0 || min > max) return 'error';
   if (min === max) return min;
 
   signs = parseInt(signs);
 
-  // if Integer is required, then use function for Integers
+  // если надо целое, то отдельная функция
   if (signs === 0) {
     return getRandomIntInclusive(min, max);
   }
 
-  // return Float
-  return (Math.random() * (max - min) + min).toFixed(signs);
+  return parseFloat((Math.random() * (max - min) + min).toFixed(signs));
 }
+
+// чтобы Линтер не выдавал ошибку
+getRandomFloat(1, 10, 2);
 
 
 
