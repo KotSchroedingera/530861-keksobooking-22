d1f33ac77a60ba2930243ec266d7ac12988a5a00
KotSchroedingera <79150323671@ya.ru>
2021-03-07 23:12:44 +0300

    Сделал модуль 5-2
    Валидация полей Тип, Цена, Чекин, Чекаут.

--- js/form.js
+++ js/form.js
@@ -1,25 +1,34 @@
 'use strict'
 
+import { setColor } from './util.js';
+
 const type = document.querySelector('#type');
 const price = document.querySelector('#price');
+const priceLabel = document.querySelector('.ad-form__label[for="price"]');
 const pricesList = {
   'bungalow' : 0,
   'flat' : 1000,
   'house' : 5000,
   'palace' : 10000,
 }
-
-const timein = document.querySelector('#timein');
-const timeout = document.querySelector('#timeout');
-
-type.addEventListener('change', () => {
+const validatePrice = () => {
   for (let key in pricesList) {
     if (key === type.value) {
       price.placeholder = pricesList[key];
       price.min = pricesList[key];
     }
   }
-});
+  const priceValidationMessage = `Для типа '${type.selectedOptions[0].innerText}' цена от ${price.min} руб.`;
+  price.setCustomValidity(priceValidationMessage);
+  +price.value < +price.min ? setColor(priceLabel, 'red') : setColor(priceLabel, 'inherit');
+}
+
+if (!price.min) validatePrice();
+type.addEventListener('change', () => validatePrice());
+price.addEventListener('input', () => validatePrice());
+
+const timein = document.querySelector('#timein');
+const timeout = document.querySelector('#timeout');
 
 timein.addEventListener('change', () => {
   timeout.value = timein.value;
@@ -29,5 +38,3 @@ timeout.addEventListener('change', () => {
   timein.value = timeout.value;
 });
 
-const priceValidationMessage = `Для типа '${type.selectedOptions[0].innerText}' цена от ${price.min}`;
-price.setCustomValidity(priceValidationMessage);
