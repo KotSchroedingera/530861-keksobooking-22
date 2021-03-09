8d9464b92d80802701cd5f6ef58e2da03206ca58
KotSchroedingera <79150323671@ya.ru>
2021-03-09 23:52:53 +0300

    Почти доделал
    todo: проверить debounce

--- js/util.js
+++ js/util.js
@@ -75,6 +75,21 @@ const setColor = (elem, color) => {
   elem.style.color = color;
 }
 
+const debounce = (func, wait, immediate) => {
+  let timeout;
+  return () => {
+    let context = this, args = arguments;
+    let later = () => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
 export {
   getRandomIntInclusive,
   getRandomFloat,
@@ -85,6 +100,7 @@ export {
   changeElementContent,
   isArrayEmpty,
   removeNodeIfEmpty,
-  setColor
+  setColor,
+  debounce
 };
 
