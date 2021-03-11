cae54fe1220f0b09356ebe1fe78e9fb8a31195e7
kotschroedingera <79150323671@ya.ru>
2021-03-11 17:58:39 +0300

    Сделал множественную загрузку фото.

--- js/file-reader.js
+++ js/file-reader.js
@@ -29,30 +29,23 @@ const photoPreview = document.querySelector('.ad-form__photo');
 
 const previewPhotos = () => {
   const photos = photoChooser.files;
-  const photo = photoChooser.files[0];
-  const checkImage = () => {
+  const checkImage = image => {
     for (let type of FILE_TYPES) {
-      if (photo.name.match(type)) return true;
+      if (image.name.match(type)) return true;
     }
     return false;
   }
-  if (checkImage()) {
-    const reader = new FileReader();
-    reader.readAsDataURL(photo);
-    reader.addEventListener('load', () => {
-      photoPreview.style.backgroundImage = reader.result;
-    });
+  for (let photo of photos) {
+    if (checkImage(photo)) {
+      const reader = new FileReader();
+      reader.readAsDataURL(photo);
+      reader.addEventListener('load', () => {
+        photoPreview.style.width = 'inherit';
+        photoPreview.style.display = 'contents';
+        photoPreview.insertAdjacentHTML('beforeend', `<img style='margin-right: 10px; margin-bottom: 10px; display: inline-block; background-color: #e4e4de' src='${reader.result}' width='70' height='70'>`);
+      });
+    }
   }
 }
 
 photoChooser.addEventListener('change', previewPhotos);
-
-
-
-
-
-
-
-
-
-
