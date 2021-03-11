a73d5b7c795e3aa5fa536b0ce471557aeb24d302
KotSchroedingera <79150323671@ya.ru>
2021-03-11 23:33:37 +0300

    Сделал.
    1. Превью аватара
    2. Превью фотографий (множественный выбор)

--- js/file-reader.js
+++ js/file-reader.js
@@ -1,51 +1,60 @@
 'use strict'
 
-const avatarChooser = document.querySelector('#avatar');
-const avatarPreview = document.querySelector('.ad-form-header__preview img');
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
-
-const previewAvatar = () => {
-  const image = avatarChooser.files[0];
-  const checkImage = () => {
-    for (let type of FILE_TYPES) {
-      if (image.name.match(type)) return true;
-    }
-    return false;
+const checkFile = (file, types) => {
+  for (let type of types) {
+    if (file.name.match(type)) return true;
   }
-  if (checkImage()) {
+  return false;
+}
+
+const previewAvatar = config => {
+  const {chooser, preview} = config;
+  const image = chooser.files[0];
+  if (checkFile(image, FILE_TYPES)) {
     const reader = new FileReader();
     reader.readAsDataURL(image);
     reader.addEventListener('load', () => {
-      avatarPreview.src = reader.result;
+      preview.src = reader.result;
     });
   }
 }
 
-avatarChooser.addEventListener('change', previewAvatar);
+const avatarConfig = {
+  chooser: document.querySelector('#avatar'),
+  preview: document.querySelector('.ad-form-header__preview img'),
+}
 
+avatarConfig.chooser.addEventListener('change', previewAvatar.bind(null, avatarConfig));
 
-const photoChooser = document.querySelector('#images');
-const photoPreview = document.querySelector('.ad-form__photo');
+const photosConfig = {
+  chooser: document.querySelector('#images'),
+  preview: document.querySelector('.ad-form__photo'),
+}
 
-const previewPhotos = () => {
-  const photos = photoChooser.files;
-  const checkImage = image => {
-    for (let type of FILE_TYPES) {
-      if (image.name.match(type)) return true;
-    }
-    return false;
-  }
+const previewPhotos = config => {
+  const {chooser, preview} = config;
+  const photos = chooser.files;
   for (let photo of photos) {
-    if (checkImage(photo)) {
+    if (checkFile(photo, FILE_TYPES)) {
       const reader = new FileReader();
       reader.readAsDataURL(photo);
       reader.addEventListener('load', () => {
-        photoPreview.style.width = 'inherit';
-        photoPreview.style.display = 'contents';
-        photoPreview.insertAdjacentHTML('beforeend', `<img style='margin-right: 10px; margin-bottom: 10px; display: inline-block; background-color: #e4e4de' src='${reader.result}' width='70' height='70'>`);
+        preview.style.width = 'inherit';
+        preview.style.display = 'contents';
+        preview.insertAdjacentHTML('beforeend',
+          `<img style='
+          margin-right: 10px;
+          margin-bottom: 10px;
+          display: inline-block;
+          background-color: #e4e4de;
+          border-radius: 5px;'
+          src='${reader.result}'
+          width='70'
+          height='70'>`);
       });
     }
   }
 }
 
-photoChooser.addEventListener('change', previewPhotos);
+photosConfig.chooser.addEventListener('change', previewPhotos.bind(null, photosConfig));
